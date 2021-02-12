5f99ed6d6faaeb4290d089a56a4041bda47503e1
HyeonSeoJeong <hsjeong@inthecore.co.kr>
2021-02-12 19:07:10 +0900

    루트 저장 및 호출 개발

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,10 +15,8 @@
       disableDefaultUi: false
     }"
     >
-      <GmapMarker
-        :position="{lat: mymarker.lat, lng: mymarker.lng}"
-      />
-      <gmap-polyline :path.sync="route" :options="{ strokeColor:'#008000'}"></gmap-polyline>
+      <GmapMarker :position="{lat: mymarker.lat, lng: mymarker.lng}" />
+      <gmap-polyline :path.sync="route" :options="{ strokeColor:'#008000'}" />
     </GmapMap>
     <div>
       lat: {{mymarker.lat}} --- lng: {{mymarker.lng}}
@@ -27,7 +25,12 @@
       <button @click="myposition">시작</button>
       <button @click="clearWatch">끝</button>
     </div>
-    <div>{{route}}</div>
+    <div>
+      <div>저장된 루트 {{routeStorage.length}}개</div>
+      <div>
+        <div v-for="(item, idx) in routeStorage" :key="idx" @click="showRoute(item)">루트{{idx + 1}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -41,6 +44,7 @@ export default {
         lng : 127.14162849294125
       },
       route: [],
+      routeStorage: [],
       watchID: null
     }
   },
@@ -53,6 +57,7 @@ export default {
   methods: {
     myposition : function() {
       if (navigator.geolocation) {
+        this.route = []
         this.watchID = navigator.geolocation.watchPosition(this.successPosition, this.failurePosition,
         {
           enableHighAccuracy: false,
@@ -75,6 +80,16 @@ export default {
     },
     clearWatch() {
       navigator.geolocation.clearWatch(this.watchID)
+      if(confirm('루트를 저장하시겠습니까?')) {
+        this.routeStorage.push(this.route)
+        this.route = []
+        alert('루트 저장 완료')
+      } else {
+        alert('루트가 삭제되었습니다.')
+      }
+    },
+    showRoute(route){
+      this.route = route
     }
   }
 }
